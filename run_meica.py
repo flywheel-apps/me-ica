bfa7b4fb580a3aa3e7834846e025ae14f2af38c9
Michael Perry <lmperry@stanford.edu>
2019-02-15 10:51:20 -0800

    use zipfile lib to zip output dir

--- run_meica.py
+++ run_meica.py
@@ -6,6 +6,7 @@ import csv
 import json
 import shutil
 import string
+import zipfile
 import logging
 import datetime
 import flywheel
@@ -13,6 +14,63 @@ from pprint import pprint
 
 logging.basicConfig()
 log = logging.getLogger('MEICA')
+
+
+def zipdir(dirPath=None, zipFilePath=None, includeDirInZip=True, deflate=True):
+    """Create a zip archive from a directory.
+
+    Note that this function is designed to put files in the zip archive with
+    either no parent directory or just one parent directory, so it will trim any
+    leading directories in the filesystem paths and not include them inside the
+    zip archive paths. This is generally the case when you want to just take a
+    directory and make it into a zip file that can be extracted in different
+    locations.
+
+    Keyword arguments:
+
+    dirPath -- string path to the directory to archive. This is the only
+    required argument. It can be absolute or relative, but only one or zero
+    leading directories will be included in the zip archive.
+
+    zipFilePath -- string path to the output zip file. This can be an absolute
+    or relative path. If the zip file already exists, it will be updated. If
+    not, it will be created. If you want to replace it from scratch, delete it
+    prior to calling this function. (default is computed as dirPath + ".zip")
+
+    includeDirInZip -- boolean indicating whether the top level directory should
+    be included in the archive or omitted. (default True)
+
+"""
+    if deflate:
+        mode = zipfile.ZIP_DEFLATED
+    else:
+        mode = zipfile.ZIP_STORED
+    if not zipFilePath:
+        zipFilePath = dirPath + ".zip"
+    if not os.path.isdir(dirPath):
+        raise OSError("dirPath argument must point to a directory. "
+            "'%s' does not." % dirPath)
+    parentDir, dirToZip = os.path.split(dirPath)
+    #Little nested function to prepare the proper archive path
+    def trimPath(path):
+        archivePath = path.replace(parentDir, "", 1)
+        if parentDir:
+            archivePath = archivePath.replace(os.path.sep, "", 1)
+        if not includeDirInZip:
+            archivePath = archivePath.replace(dirToZip + os.path.sep, "", 1)
+        return os.path.normcase(archivePath)
+
+    outFile = zipfile.ZipFile(zipFilePath, "w", mode, allowZip64=True)
+    for (archiveDirPath, dirNames, fileNames) in os.walk(dirPath):
+        for fileName in fileNames:
+            filePath = os.path.join(archiveDirPath, fileName)
+            outFile.write(filePath, trimPath(filePath))
+        #Make sure we get empty directories as well
+        if not fileNames and not dirNames:
+            zipInfo = zipfile.ZipInfo(trimPath(archiveDirPath) + "/")
+            outFile.writestr(zipInfo, "")
+    outFile.close()
+
 
 def get_meica_data(config, output_directory='/flywheel/v0/output'):
     """
@@ -124,9 +182,10 @@ if __name__ == '__main__':
                         if os.path.isdir(os.path.join(output_directory, x))
                 ]
         for d in dirs:
-            out_zip = os.path.join(output_directory, os.path.basename(d))
+            out_zip = os.path.join(output_directory, os.path.basename(d) + '.zip')
             log.info('Generating %s... ' % (out_zip))
-            shutil.make_archive(out_zip, 'zip', root_dir=output_directory, base_dir=os.path.basename(d), verbose=True)
+            # shutil.make_archive(out_zip, 'zip', root_dir=output_directory, base_dir=os.path.basename(d), verbose=True)
+            zipdir(d, out_zip, os.path.basename(d))
             shutil.rmtree(d)
 
     log.info('Done: %s' % datetime.datetime.utcnow())
